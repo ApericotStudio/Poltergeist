a4ec70dd2d24b900669bce6f7d0d285be806aba5
Liang-Tong <kiwilt@icloud.com>
2023-10-30 14:45:56 +0100

    trajectory line

--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class Throwing : MonoBehaviour
 {
+    [Header("Throw Controls")]
+    [SerializeField] public bool throwMode;
+    [SerializeField] private float throwForce;
+    [SerializeField] private LineRenderer lineRenderer;
+    private Vector3 releasePosition;
+
+    [Header("Display Controls")]
+    [SerializeField] [Range(10, 100)] private int linePoints = 25;
+    [SerializeField] [Range(0.01f, 0.25f)] private float timeBetweenPoints = 0.1f;
+    private LayerMask throwLayerMask;
+
+    private Camera cam;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
+        cam = Camera.main;
+
+        int throwLayer = this.gameObject.layer;
+        for (int i = 0; i < 32; i++)
+        {
+            if (!Physics.GetIgnoreLayerCollision(throwLayer, i))
+            {
+                throwLayerMask |= 1 << i;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (throwMode)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                DrawProjection();
+            } else
+            {
+                lineRenderer.enabled = false;
+            }
+            
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                ThrowObject();
+            }
+        }
+
+
+
+    }
+    private void ThrowObject()
+    {
+        rb.AddForce(cam.transform.forward * throwForce, ForceMode.Impulse);
+    }
+
+    private void DrawProjection()
+    {
+        releasePosition = transform.position;
+        lineRenderer.enabled = true;
+        lineRenderer.positionCount = Mathf.CeilToInt(linePoints / timeBetweenPoints) + 1;
+        Vector3 startPosition = releasePosition;
+        Vector3 startVelocity = throwForce * cam.transform.forward / rb.mass;
+        int i = 0;
+        lineRenderer.SetPosition(i, startPosition);
+        for (float time = 0; time < linePoints; time += timeBetweenPoints)
+        {
+            i++;
+            Vector3 point = startPosition + time * startVelocity;
+
+            //Trajectory formula here
+            point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time);
+
+            lineRenderer.SetPosition(i, point);
+
+            Vector3 lastPosition = lineRenderer.GetPosition(i - 1);
+
+            if (Physics.Raycast(lastPosition, (point - lastPosition).normalized, out RaycastHit hit, (point - lastPosition).magnitude, throwLayerMask))
+            {
+                lineRenderer.SetPosition(i, hit.point);
+                lineRenderer.positionCount = i + 1;
+                return;
+            }
+        }
     }
 }
