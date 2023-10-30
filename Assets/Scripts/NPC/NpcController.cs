f81efd0d17bf9445ad8edef838cde6cdbc3691dc
Dylan Smit <sircodeye@gmail.com>
2023-10-30 12:45:56 +0100

    Feat: added anxiety stat to npc

--- Assets/Scripts/NPC/NpcController.cs
+++ Assets/Scripts/NPC/NpcController.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
-public enum NpcState
-{
-    Roam,
-    Frightened
-}
 public class NpcController : MonoBehaviour
 {
     [Header("NPC Settings")]
@@ -25,14 +21,20 @@ public class NpcController : MonoBehaviour
     [Tooltip("The speed the NPC will move when frightened.")]
     [Range(2f, 10f)]
     public float FrightenedSpeed = 5.335f;
-    [Tooltip("Enable this to make the NPC run away.")]
-    public bool IsFrightened;
+    [Tooltip("The anxiety value of the NPC.")]
+    [SerializeField]
+    [Range(0f, 100f)]
+    private float _anxietyValue = 0f;
+    [SerializeField]
+    [Tooltip("The event that will be invoked when the anxiety value changes.")]
+    private UnityEvent<float> _onAnxietyValueChange;
+
     [Tooltip("The audio clips that will be played when the NPC moves.")]
     public AudioClip[] FootstepAudioClips;
     [Tooltip("The volume of the footstep audio clips.")]
     [Range(0f, 1f)]
     public float footstepVolume = 0.5f;
-
+    
     private NavMeshAgent _navMeshAgent;
     private INpcState _currentState;
     private bool _ranAway;
@@ -45,26 +47,35 @@ public class NpcController : MonoBehaviour
     public NavMeshAgent NavMeshAgent { get => _navMeshAgent; set => _navMeshAgent = value; }
     public INpcState CurrentState { get => _currentState; set => _currentState = value; }
     public bool RanAway { get => _ranAway; set => _ranAway = value; }
+    public float AnxietyValue
+    { 
+        get => _anxietyValue; 
+        set {
+            _anxietyValue = value;
+            _onAnxietyValueChange?.Invoke(_anxietyValue);
+        }  
+    }
 
     private void Awake()
     {
         NavMeshAgent = GetComponent<NavMeshAgent>();
         InitializeAnimator();
         _currentState = new RoamState(this);
+        _onAnxietyValueChange = new UnityEvent<float>();
     }
 
     private void Update()
     {
-        if(IsFrightened && _currentState.GetType() != typeof(FrightenedState))
+        if(AnxietyValue == 100f && _currentState.GetType() != typeof(FrightenedState))
         {
-            TurnFrightened();
+            GetScared();
         }
         Animate();
         _currentState.Execute();
 
     }
 
-    private void TurnFrightened()
+    private void GetScared()
     {
         _currentState = new FrightenedState(this);
     }
