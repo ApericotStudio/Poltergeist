262c3781ec24a2bd178d9c837a7b238c34de5dfd
ThijsvanDijk <thijs3214@gmail.com>
2023-10-31 17:20:33 +0100

    Fixed comments

--- Assets/Scripts/UI/TutorialText.cs
+++ Assets/Scripts/UI/TutorialText.cs
@@ -1,5 +1,5 @@
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(TMP_Text))]
 public class TutorialText : MonoBehaviour
@@ -11,11 +11,20 @@ public class TutorialText : MonoBehaviour
         text = GetComponent<TMP_Text>();
     }
 
+    /// <summary>
+    /// Updates the text displayed by the Text object.
+    /// Call this function on an event.
+    /// </summary>
+    /// <param name="text">The new text for the </param>
     public void UpdateText(string text)
     {
         this.text.text = text;
     }
     
+    /// <summary>
+    /// Hides the text.
+    /// Call this function on an event.
+    /// </summary>
     public void Hide()
     {
         text.gameObject.SetActive(false);
