b883211d01ad94f83e6706d509db9b5af07faa47
Liang-Tong <kiwilt@icloud.com>
2023-11-01 17:55:34 +0100

    move works

--- Assets/Scripts/Ghost/ThirdPersonController.cs
+++ Assets/Scripts/Ghost/ThirdPersonController.cs
@@ -74,6 +74,8 @@ namespace StarterAssets
         private float _animationBlend;
         private float _targetRotation = 0.0f;
         private float _rotationVelocity;
+        [SerializeField] float rotationSpeed = 10;
+        public bool freeze;
 
         // animation IDs
         private int _animIDSpeed;
@@ -129,7 +131,8 @@ namespace StarterAssets
 #else
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
-
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
             AssignAnimationIDs();
         }
 
@@ -138,7 +141,17 @@ namespace StarterAssets
             _hasAnimator = TryGetComponent(out _animator);
 
             GroundedCheck();
-            Move();
+            if (!freeze)
+            {
+                Look();
+                Move();
+            }
+        }
+
+        private void Look()
+        {
+            float playerRotate = rotationSpeed * Input.GetAxis("Mouse X");
+            transform.Rotate(0, playerRotate, 0);
         }
 
         private void LateUpdate()
--- Assets/Scripts/Possession/PossessionController.cs
+++ Assets/Scripts/Possession/PossessionController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Cinemachine;
+using StarterAssets;
 
 public class PossessionController : MonoBehaviour
 {
@@ -7,11 +9,14 @@ public class PossessionController : MonoBehaviour
 
     private IPossessable currentPossession;
     private Camera mainCamera;
+    private CinemachineVirtualCamera virtcam;
+    private ThirdPersonController controller;
 
     private void Start()
     {
         mainCamera = Camera.main;
-        currentPossession = GetComponent<IPossessable>();
+        virtcam = this.gameObject.GetComponentInChildren<CinemachineVirtualCamera>();
+        controller = this.gameObject.GetComponent<ThirdPersonController>();
     }
 
     private void Update()
@@ -30,17 +35,23 @@ public class PossessionController : MonoBehaviour
         IPossessable possessable = LookForPossessableObject();
         if (possessable == null)
         {
-            Unpossess();
-            return;
+            if (currentPossession != null)
+            {
+                Unpossess();
+                return;
+            }
         }
-        currentPossession.Unpossess();
+        controller.freeze = true;
+        this.virtcam.Priority = 0;
         possessable.Possess();
         currentPossession = possessable;
     }
 
     private void Unpossess()
     {
+        this.virtcam.Priority = 1;
         currentPossession.Unpossess();
+        controller.freeze = false;
     }
 
     /// <summary>
