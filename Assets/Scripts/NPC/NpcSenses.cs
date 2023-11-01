eb47012ce70a47ed27fa765abf2cd7fbaf880c65
Dylan Smit <sircodeye@gmail.com>
2023-11-01 15:24:00 +0100

    Change: changed access modifiers in NpcSenses to private

--- Assets/Scripts/NPC/NpcSenses.cs
+++ Assets/Scripts/NPC/NpcSenses.cs
@@ -11,39 +11,45 @@ public class NpcSenses : MonoBehaviour
     [Header("Sight Settings")]
     [Range(0, 360)]
     [Tooltip("The angle of the NPC's field of view.")]
-    public float FieldOfViewAngle = 110f;
+    [SerializeField]
+    private float _fieldOfViewAngle = 110f;
     [Tooltip("The distance that the NPC can see.")]
     [Range(0, 10)]
-    public float SightRange = 10f;
-
+    [SerializeField]
+    private float _sightRange = 10f;
     [Header("Auditory Settings")]
     [Tooltip("The distance that the NPC can hear.")]
     [Range(0, 20)]
-    public float AuditoryRange = 10f;
-
+    [SerializeField]
+    private float _auditoryRange = 10f;
     [Header("Senses Layers")]
     [Tooltip("The target layers that the NPC can see and hear.")]
-    public LayerMask TargetMask;
+    [SerializeField]
+    private LayerMask _targetMask;
     [Tooltip("The obstacle layers that block the NPC's vision.")]
-    public LayerMask ObstacleMask;
+    [SerializeField]
+    private LayerMask _obstacleMask;
 
     private float _detectionRange;
     public float DetectionRange 
     { 
         get {
-            _detectionRange = Math.Max(AuditoryRange, SightRange);
+            _detectionRange = Math.Max(_auditoryRange, _sightRange);
             return _detectionRange;
         }
     }
     private NpcController _npcController;
     private List<Clutter> _detectedClutter = new();
     public List<Clutter> DetectedClutter { get => _detectedClutter; set => _detectedClutter = value; }
+    public float SightRange{ get => _sightRange; set => _sightRange = value; }
+    public float FieldOfViewAngle { get => _fieldOfViewAngle; set => _fieldOfViewAngle = value; }
+    public float AuditoryRange { get => _auditoryRange; set => _auditoryRange = value; }
 
     private void Awake()
     {
         _npcController = GetComponent<NpcController>();
         StartCoroutine (DetectTargetsWithDelay(.2f));
-        _detectionRange = Math.Max(AuditoryRange, SightRange);
+        _detectionRange = Math.Max(_auditoryRange, _sightRange);
     }
     /// <summary>
     /// Detects targets in the NPC's detection radius with a delay.
@@ -69,7 +75,7 @@ public class NpcSenses : MonoBehaviour
     {
         ClearDetectedClutter();
 
-        Collider[] targetsInDetectionRadius = Physics.OverlapSphere(transform.position, DetectionRange, TargetMask);
+        Collider[] targetsInDetectionRadius = Physics.OverlapSphere(transform.position, DetectionRange, _targetMask);
         for (int i = 0; i < targetsInDetectionRadius.Length; i++)
         {
             Transform target = targetsInDetectionRadius[i].transform;
@@ -77,16 +83,16 @@ public class NpcSenses : MonoBehaviour
             {
                 Vector3 dirToTarget = (target.position - transform.position).normalized;
                 float dstToTarget = Vector3.Distance(transform.position, target.position);
-                if (Physics.Raycast(transform.position, dirToTarget, dstToTarget, ObstacleMask))
+                if (Physics.Raycast(transform.position, dirToTarget, dstToTarget, _obstacleMask))
                 {
                     return;
                 }
-                if (Vector3.Angle (transform.forward, dirToTarget) < FieldOfViewAngle / 2) {
+                if (Vector3.Angle (transform.forward, dirToTarget) < _fieldOfViewAngle / 2) {
                     clutter.IsVisible = true;
                     DetectedClutter.Add(clutter);
                     return;
                 }
-                if (!(dstToTarget <= AuditoryRange))
+                if (!(dstToTarget <= _auditoryRange))
                     return;
                 clutter.IsAudible = true;
                 DetectedClutter.Add(clutter);
