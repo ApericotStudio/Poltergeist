a7072f63d87d32f4446800636fb3c0d1c2707c9a
Liang-Tong <kiwilt@icloud.com>
2023-11-09 13:29:53 +0100

    private variables with underscore

--- Assets/Scripts/Possession/ClutterCamera.cs
+++ Assets/Scripts/Possession/ClutterCamera.cs
@@ -6,21 +6,21 @@ using UnityEngine.InputSystem;
 
 public class ClutterCamera : MonoBehaviour
 {
-    [SerializeField] private bool LockCameraPosition = false;
+    [SerializeField] public bool LockCameraPosition = false;
     [SerializeField] public GameObject CinemachineCameraTarget;
-    [SerializeField] private float Sensitivity = 1f;
+    [SerializeField] private float _sensitivity = 1f;
     [Tooltip("How far in degrees can you move the camera up")]
-    [SerializeField] private float TopClamp = 70.0f;
+    [SerializeField] private float _topClamp = 70.0f;
     [Tooltip("How far in degrees can you move the camera down")]
-    [SerializeField] private float BottomClamp = -30.0f;
+    [SerializeField] private float _bottomClamp = -30.0f;
     [Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
-    [SerializeField] private float CameraAngleOverride = 0.0f;
+    [SerializeField] private float _cameraAngleOverride = 0.0f;
     private PlayerInput _playerInput;
     private StarterAssetsInputs _input;
     private const float _threshold = 0.01f;
     private float _cinemachineTargetYaw;
     private float _cinemachineTargetPitch;
-    private bool IsCurrentDeviceMouse
+    private bool _isCurrentDeviceMouse
     {
         get
         {
@@ -50,18 +50,18 @@ public class ClutterCamera : MonoBehaviour
         if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
         {
             //Don't multiply mouse input by Time.deltaTime;
-            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+            float deltaTimeMultiplier = _isCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 
-            _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * Sensitivity;
-            _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier * Sensitivity;
+            _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * _sensitivity;
+            _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier * _sensitivity;
         }
 
         // clamp our rotations so our values are limited 360 degrees
         _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
-        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, _bottomClamp, _topClamp);
 
         // Cinemachine will follow this target
-        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + _cameraAngleOverride,
             _cinemachineTargetYaw, 0.0f);
     }
     private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
