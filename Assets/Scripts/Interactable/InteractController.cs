4d0262b9bb36b42c3e137abde911acdf46b937a0
Jeroen <s1137642@student.hsleiden.nl>
2023-11-09 13:57:58 +0100

    change: added header and changed interact button to f

--- Assets/Scripts/Interactable/InteractController.cs
+++ Assets/Scripts/Interactable/InteractController.cs
@@ -21,7 +21,7 @@ public class InteractController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             Interact();
         }
--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 public class Interactable : MonoBehaviour
 {
+    [Header("Adjustable variables")]
     [Tooltip("Interactable is reusable")]
     [SerializeField] private bool singleUse = false;
     private bool used = false;
