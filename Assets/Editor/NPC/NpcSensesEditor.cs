400c99af65adf46aad60fdbe05014bbc51f2072f
Dylan <95217570+SirCodeye@users.noreply.github.com>
2023-11-10 15:01:20 +0100

    Change: improved NPC detection visual feedback in scene view

--- Assets/Editor/NPC/NpcSensesEditor.cs
+++ Assets/Editor/NPC/NpcSensesEditor.cs
@@ -11,39 +11,27 @@ public class NpcSensesEditor : Editor
     private NpcSenses _npcSenses;
 	private void OnSceneGUI() {
 		_npcSenses = (NpcSenses)target;
-        DrawDetectionRange();
-        DrawFieldOfView();
         DrawHearingRadius();
+        DrawFieldOfView();
         DrawDetectionLine();
 	}
-    /// <summary>
-    /// Draws the NPC's detection range on screen, which is the maximum of the NPC's hearing radius and field of view.
-    /// </summary>
-    private void DrawDetectionRange()
-    {
-        Handles.color = Color.gray;
-        Handles.DrawWireArc(_npcSenses.transform.position, Vector3.up, Vector3.forward, 360,  Math.Max(_npcSenses.AuditoryRange, _npcSenses.DetectionRange));
-    }
     /// <summary>
     /// Draws the NPC's hearing radius on screen, which is the NPC's auditory range.
     /// </summary>
     private void DrawHearingRadius()
     {
-        Handles.color = Color.yellow;
-        Handles.DrawWireArc(_npcSenses.transform.position, Vector3.up, Vector3.forward, 360, _npcSenses.AuditoryRange);
+        Handles.color = new Color(1, 1, 0, 0.1f); // Yellow color with 50% transparency
+        Handles.DrawSolidArc(_npcSenses.transform.position, Vector3.up, Vector3.forward, 360, _npcSenses.AuditoryRange);
     }
     /// <summary>
     /// Draws the NPC's field of view on screen, which is the NPC's sight range and field of view angle.
     /// </summary>
     private void DrawFieldOfView()
     {
-        Vector3 viewAngleA = _npcSenses.DirFromAngle (-_npcSenses.FieldOfViewAngle / 2, false);
-		Vector3 viewAngleB = _npcSenses.DirFromAngle (_npcSenses.FieldOfViewAngle / 2, false);
+        Vector3 viewAngle = _npcSenses.DirFromAngle(-_npcSenses.FieldOfViewAngle / 2, false);
 
-        Handles.color = Color.blue;
-		Handles.DrawLine (_npcSenses.transform.position, _npcSenses.transform.position + viewAngleA * _npcSenses.SightRange);
-		Handles.DrawLine (_npcSenses.transform.position, _npcSenses.transform.position + viewAngleB * _npcSenses.SightRange);
-        Handles.DrawWireArc(_npcSenses.transform.position, Vector3.up, viewAngleA * _npcSenses.SightRange, _npcSenses.FieldOfViewAngle, _npcSenses.SightRange);
+        Handles.color = new Color(0, 0, 1, 0.2f); // Blue color with 50% transparency
+        Handles.DrawSolidArc(_npcSenses.transform.position, Vector3.up, viewAngle, _npcSenses.FieldOfViewAngle, _npcSenses.SightRange);
     }
     /// <summary>
     /// Draws a line from the NPC to the detected clutter. The line is red if the clutter is visible, and yellow if the clutter is audible.
