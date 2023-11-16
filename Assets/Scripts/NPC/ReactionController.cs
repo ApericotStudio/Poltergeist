34d599d158fa7b476e36a65ee434495ab6ce99f6
Dylan Smit <sircodeye@gmail.com>
2023-11-16 13:13:50 +0100

    Feat: npc's can react with a question mark when investigating

--- Assets/Scripts/NPC/ReactionController.cs
+++ Assets/Scripts/NPC/ReactionController.cs
@@ -14,6 +14,8 @@ public class ReactionController : MonoBehaviour
     private Sprite _mediumFearSprite;
     [Tooltip("The sprite that will be displayed when the fear value is high."), SerializeField]
     private Sprite _highFearSprite;
+    [Tooltip("The sprite that will be displayed when the NPC is investigating."), SerializeField]
+    private Sprite _investigateSprite;
 
     private NpcController _npcController;
     private Canvas _reactionCanvas;
@@ -38,6 +40,11 @@ public class ReactionController : MonoBehaviour
     /// <param name="fear">The current fear value of the NPC.</param>
     private void SetNpcReaction(float fear)
     {
+        if(_npcController.CurrentState == _npcController.InvestigateState && fear <= 75f)
+        {
+            _reactionImage.sprite = _investigateSprite;
+            return;
+        }
         if(fear <= 25f)
         {
             _reactionImage.sprite = _lowFearSprite;
