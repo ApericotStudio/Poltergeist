2f8599025c98a90cd15c66b5c4f2b150f5b27f63
Dylan Smit <sircodeye@gmail.com>
2023-11-17 15:24:46 +0100

    Change: the player goes to the closest point on the target collider compared to the transform position

--- Assets/Scripts/NPC/States/InvestigateState.cs
+++ Assets/Scripts/NPC/States/InvestigateState.cs
@@ -5,6 +5,8 @@ public class InvestigateState : INpcState
 {
     private readonly NpcController _npcController;
 
+    private Collider _investigateTargetCollider;
+
     public InvestigateState(NpcController npcController)
     {
         _npcController = npcController;
@@ -12,6 +14,7 @@ public class InvestigateState : INpcState
 
     public void Handle()
     {
+        _investigateTargetCollider = _npcController.InvestigateTarget.GetComponent<Collider>();
         _npcController.StartCoroutine(InvestigateCoroutine());
     }
 
@@ -24,15 +27,16 @@ public class InvestigateState : INpcState
         _npcController.NavMeshAgent.speed = _npcController.InvestigatingSpeed;
         _npcController.NavMeshAgent.stoppingDistance = 2f;
         _npcController.NpcAudioSource.PlayOneShot(_npcController.InvestigateAudioClip);
-        _npcController.NavMeshAgent.SetDestination(_npcController.InvestigateTarget.position);
+        _npcController.NavMeshAgent.SetDestination(NearestPointOnTargetFromPlayer());
         _npcController.FearReductionHasCooldown = true;
+
         // This while loop continues as long as the NPC's navigation path is still being calculated (pathPending) 
         // or the remaining distance to the target is greater than the stopping distance. 
         // This ensures the NPC continues moving until it has reached its destination.
 
         while(_npcController.NavMeshAgent.pathPending || _npcController.NavMeshAgent.remainingDistance > _npcController.NavMeshAgent.stoppingDistance)
         {
-            _npcController.NavMeshAgent.SetDestination(_npcController.InvestigateTarget.position);
+            _npcController.NavMeshAgent.SetDestination(NearestPointOnTargetFromPlayer());
             yield return new WaitForSeconds(0.2f);
         }
 
@@ -43,5 +47,13 @@ public class InvestigateState : INpcState
         _npcController.NpcAudioSource.PlayOneShot(_npcController.InvestigateEndAudioClip);
         _npcController.NavMeshAgent.SetDestination(_npcController.RoamTargetLocation.position);
         yield break;
+    }
+
+    /// <summary>
+    /// Returns the nearest point on the target from the player.
+    /// </summary>
+    private Vector3 NearestPointOnTargetFromPlayer()
+    {
+        return _investigateTargetCollider.ClosestPoint(_npcController.transform.position);
     }
 }
