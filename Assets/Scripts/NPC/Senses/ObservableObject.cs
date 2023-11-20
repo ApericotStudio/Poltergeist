a073dc3d6acfb4734271bbf449c280b301f72b8b
Liang-Tong <kiwilt@icloud.com>
2023-11-20 12:42:25 +0100

    fixed bug when unpossessing when breaking an object

--- Assets/Scripts/NPC/Senses/ObservableObject.cs
+++ Assets/Scripts/NPC/Senses/ObservableObject.cs
@@ -56,9 +56,9 @@ public class ObservableObject : MonoBehaviour, IObservableObject
 
     public void NotifyObservers()
     {
-        foreach (IObserver observer in _observers)
+        for (int i = 0; i < _observers.Count; i++)
         {
-            observer.OnNotify(this);
+            _observers[i].OnNotify(this);
         }
     }
 
