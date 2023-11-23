bd96898a3cd2c5fe147969a2fbf6707afa55c24e
Jeroen <s1137642@student.hsleiden.nl>
2023-11-23 17:40:49 +0100

    change: it's finished, just need to put the tracker on the camera

--- Assets/Scripts/Player/PolterSenseController.cs
+++ Assets/Scripts/Player/PolterSenseController.cs
@@ -3,39 +3,30 @@ using UnityEngine;
 
 public class PolterSenseController : MonoBehaviour
 {
-    private List<Outline> outlinesInRange = new List<Outline>();
-    private bool isOn = false;
+    [SerializeField] private List<Outline> _outlinesInRange = new List<Outline>();
+    [SerializeField] private bool _isOn = false;
 
-    private void Update()
+    public void SetState(bool enable)
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        _isOn = enable;
+        if (enable)
         {
-            Toggle();
-        }
-    }
-
-    public void Toggle()
-    {
-        if (!isOn)
-        {
-            foreach (Outline outline in outlinesInRange)
+            foreach (Outline outline in _outlinesInRange)
             {
                 outline.enabled = true;
             }
-            isOn = !isOn;
             return;
         }
-        foreach (Outline outline in outlinesInRange)
+        foreach (Outline outline in _outlinesInRange)
         {
             outline.enabled = false;
         }
-        isOn = !isOn;
     }
 
     public void AddOutline(Outline outline)
     {
-        outlinesInRange.Add(outline);
-        if (isOn)
+        _outlinesInRange.Add(outline);
+        if (_isOn)
         {
             outline.enabled = true;
         }
@@ -43,7 +34,7 @@ public class PolterSenseController : MonoBehaviour
 
     public void RemoveOutline(Outline outline)
     {
-        outlinesInRange.Remove(outline);
+        _outlinesInRange.Remove(outline);
         outline.enabled = false;
     }
 }
