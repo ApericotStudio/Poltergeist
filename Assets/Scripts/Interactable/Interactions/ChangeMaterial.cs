4a26beead77b794ba22390366dc20638a40527ba
Hassan Mohamed <s1136140@student.hsleiden.nl>
2023-11-23 21:01:28 +0100

    feat: change image

--- Assets/Scripts/Interactable/Interactions/ChangeMaterial.cs
+++ Assets/Scripts/Interactable/Interactions/ChangeMaterial.cs
@@ -9,24 +9,26 @@ public class ChangeMaterial : MonoBehaviour
     [SerializeField]
     private Material _onMaterial;
 
-    private Material _currentMaterial;
+    private Renderer _renderer;
+
     private bool _turnedOn = false;
     private void Awake()
     {
-        _currentMaterial = GetComponent<Material>();
+        _renderer = GetComponent<Renderer>();
     }
 
     public void Change()
     {
+
         if (!_turnedOn)
         {
-            _currentMaterial = _onMaterial;
+            _renderer.material = _onMaterial;
             _turnedOn = true;
         }
 
         else
         {
-            _currentMaterial = _offMaterial;
+            _renderer.material = _offMaterial;
             _turnedOn = false;
         }
     }
