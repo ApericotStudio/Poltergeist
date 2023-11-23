68a347269f9a58b0384ffe912bc2692d8eaa33ed
Liang-Tong <kiwilt@icloud.com>
2023-11-23 16:59:17 +0100

    basic bookcase

---
+++ Assets/Scripts/Interactable/BookCase.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookCase : MonoBehaviour
+{
+    public GameObject book;
+
+    [SerializeField] private int nrOfBooks;
+    private int bookCounter = 0;
+    [SerializeField] private GameObject spawnLocation;
+    [SerializeField] private float throwForce = 500;
+    [SerializeField] private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void throwBook()
+    {
+        if (bookCounter < nrOfBooks)
+        {
+            GameObject myBook = Instantiate(book, spawnLocation.transform.position, Quaternion.identity);
+            Rigidbody rb = myBook.GetComponent<Rigidbody>();
+            rb.AddForce(direction * throwForce);
+            bookCounter++;
+        }
+    }
+}
