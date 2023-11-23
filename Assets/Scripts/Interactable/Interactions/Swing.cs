d9dfe3bc510baab3ca6dce0ecf10d909d18967f7
Hassan Mohamed <s1136140@student.hsleiden.nl>
2023-11-23 13:47:18 +0100

    feat: chandelier with realistic swing

--- Assets/Scripts/Interactable/Interactions/Swing.cs
+++ Assets/Scripts/Interactable/Interactions/Swing.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Swing : MonoBehaviour
 {
     [SerializeField]
     private Vector3 _swingDirection;
+
     public void Activate()
     {
+        // Swings object in direction of player
         gameObject.GetComponent<Rigidbody>().AddForce(_swingDirection, ForceMode.Impulse);
     }
 }
