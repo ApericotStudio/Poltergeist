1b7ff441b8a26d7c67d4f9a2001bc955517e22c3
Dylan Smit <sircodeye@gmail.com>
2023-11-24 11:01:20 +0100

    Feat: added realtor senses

---
+++ Assets/Scripts/AI/Realtor/RealtorController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RealtorController : MonoBehaviour
+{
+    [Header("Realtor Settings")]
+    [Tooltip("The speed the realtor will move when roaming."), Range(1f, 5f)]
+    public float RoamingSpeed = 2f;
+    [Tooltip("All the NPCs in the scene."), SerializeField]
+    private List<Transform> _npcs;
+    [Tooltip("The amount of time the NPC will stay around one npc origin"), Range(0f, 100f), SerializeField]
+    private float _npcOriginTimeSpent = 30f;
+    [Header("Audio Settings")]
+    [Tooltip("The audio clips that will be played when the NPC moves.")]
+    public AudioClip[] FootstepAudioClips;
+    [Tooltip("The volume of the footstep audio clips.")]
+    [Range(0f, 1f)]
+    public float FootstepVolume = 0.5f;
+    private float _npcOriginRadius = 5f;
+    private Transform _currentNpcOrigin;
+    private float _animationBlend;
+    private readonly int _animIDSpeed = Animator.StringToHash("Speed");
+    private readonly int _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+    private Animator _animator;
+
+
+    private NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _currentNpcOrigin = GetNewNpcOrigin();
+        _navMeshAgent.SetDestination(_currentNpcOrigin.position);
+        StartCoroutine(PeriodicallySetNpcOriginCoroutine());
+        StartCoroutine(RoamAroundNpc());
+    }
+
+    private void Update()
+    {
+        Animate();
+    }
+
+    private IEnumerator PeriodicallySetNpcOriginCoroutine()
+    {
+        _navMeshAgent.SetDestination(_currentNpcOrigin.position);
+        yield return new WaitUntil(() => _navMeshAgent.remainingDistance < 0.5f && !_navMeshAgent.pathPending);
+        yield return new WaitForSeconds(_npcOriginTimeSpent);
+        _currentNpcOrigin = GetNewNpcOrigin();
+    }
+
+    private IEnumerator RoamAroundNpc()
+    {
+        _navMeshAgent.stoppingDistance = 0f;
+        _navMeshAgent.speed = RoamingSpeed;
+
+        while (true)
+        {
+            if (_navMeshAgent.remainingDistance < 0.5f)
+            {
+                _navMeshAgent.SetDestination(GetRoamLocation());
+            }
+            yield return new WaitForSeconds(Random.Range(3f, 5f));
+        }
+    }
+
+    private Vector3 GetRoamLocation()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * _npcOriginRadius;
+        randomDirection += _currentNpcOrigin.position;
+        NavMesh.SamplePosition(randomDirection, out NavMeshHit hit, _npcOriginRadius, 1);
+        return hit.position;
+    }
+
+    private Transform GetNewNpcOrigin()
+    {
+        List<Transform> npcsCopy = new(_npcs);
+        npcsCopy.Remove(_currentNpcOrigin);
+        return npcsCopy[Random.Range(0, npcsCopy.Count)];
+    }
+    
+    private void Animate()
+    {
+        _animationBlend = Mathf.Lerp(_animationBlend, _navMeshAgent.velocity.magnitude, Time.deltaTime * _navMeshAgent.acceleration);
+        if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+        _animator.SetFloat(_animIDSpeed, _animationBlend);
+        _animator.SetFloat(_animIDMotionSpeed, 1f);
+    }
+    
+    private void OnFootstep(AnimationEvent animationEvent)
+    {
+        if (animationEvent.animatorClipInfo.weight > 0.5f)
+        {
+            if (FootstepAudioClips.Length > 0)
+            {
+                int index = Random.Range(0, FootstepAudioClips.Length);
+                AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.position, FootstepVolume);
+            }
+        }
+    }
+}
--- Assets/Scripts/AI/Realtor/RealtorSenses.cs
+++ Assets/Scripts/AI/Realtor/RealtorSenses.cs
@@ -4,5 +4,53 @@ using UnityEngine;
 
 public class RealtorSenses : MonoBehaviour
 {
-    
+    [Header("Detection Settings")]
+    [Tooltip("The target layer the realtor detects."), SerializeField]
+    private LayerMask _targetMask;
+    [Tooltip("The obstacle layers that block the realtor's vision."), SerializeField]
+    private LayerMask _obstacleMask;
+    [Tooltip("The distance that the realtor can detect."), Range(0, 50)]
+    public float DetectionRange = 10f;
+    private const float DetectionDelay = .2f;
+
+    [HideInInspector]
+    public List<NpcController> DetectedNpcs = new();
+
+    private void Awake()
+    {
+        StartCoroutine(DetectNpcsWithDelay(DetectionDelay));
+    }
+
+    private IEnumerator DetectNpcsWithDelay(float delay)
+    {
+        while (true)
+        {
+            DetectNpcs();
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    private void DetectNpcs()
+    {
+        DetectedNpcs.Clear();
+
+        Collider[] targetsInDetectionRadius = Physics.OverlapSphere(transform.position, DetectionRange, _targetMask);
+        for (int i = 0; i < targetsInDetectionRadius.Length; i++)
+        {
+            Collider target = targetsInDetectionRadius[i];
+            Debug.Log("Target detected");
+            bool IsNpc = target.TryGetComponent<NpcController>(out var npc);
+
+            if (!IsNpc)
+                continue;
+                Debug.Log("Npc detected");
+            Vector3 directionToTarget = (target.transform.position - transform.position).normalized;
+            float distanceToTarget = Vector3.Distance(transform.position, target.ClosestPoint(transform.position));
+
+            if (Physics.Raycast(transform.position, directionToTarget, distanceToTarget, _obstacleMask))
+                continue;
+
+            DetectedNpcs.Add(npc);
+        }
+    }
 }
