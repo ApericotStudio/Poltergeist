b7a87b2798910fbcede6ba684aee3eca74090076
Liang-Tong <kiwilt@icloud.com>
2023-11-27 14:44:46 +0100

    made door script

---
+++ Assets/Scripts/Interactable/Interactions/Door.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool IsOpen = false;
+    [SerializeField] private bool _isRotatingDoor = true;
+    [SerializeField] private float _speed = 1f;
+
+    [Header("Rotation Configs")]
+    [SerializeField] private float _rotationAmount = 90f;
+    [SerializeField] private float _forwardDirection = 0;
+
+    private Vector3 StartRotation;
+    private Vector3 Forward;
+
+    private Coroutine AnimationCoroutine;
+
+    private void Awake()
+    {
+        StartRotation = transform.rotation.eulerAngles;
+
+        Forward = transform.right;
+    }
+
+    // Start is called before the first frame update
+    public void Open(Vector3 UserPosition)
+    {
+        if (!IsOpen)
+        {
+            if (AnimationCoroutine != null)
+            {
+                StopCoroutine(AnimationCoroutine);
+            }
+
+            if (_isRotatingDoor)
+            {
+                float dot = Vector3.Dot(Forward, (UserPosition - transform.position).normalized);
+            }
+        }
+    }
+
+    private IEnumerator DoRotationOpen(float ForwardAmount)
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation;
+        if (ForwardAmount >= _forwardDirection)
+        {
+            endRotation = Quaternion.Euler(new Vector3(0, StartRotation.y - _rotationAmount, 0));
+        } else
+        {
+            endRotation = Quaternion.Euler(new Vector3(0, StartRotation.y + _rotationAmount, 0));
+        }
+
+        IsOpen = true;
+
+        float time = 0;
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, time);
+            yield return null;
+            time += Time.deltaTime * _speed;
+        }
+    }
+
+    public void Close()
+    {
+        if (IsOpen)
+        {
+            if (AnimationCoroutine != null)
+            {
+                StopCoroutine(AnimationCoroutine);
+            }
+
+            if (_isRotatingDoor)
+            {
+                AnimationCoroutine = StartCoroutine(DoRotationClose());
+            }
+        }
+    }
+
+    private IEnumerator DoRotationClose()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(StartRotation);
+
+        IsOpen = false;
+
+        float time = 0;
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, time);
+            yield return null;
+            time += Time.deltaTime * _speed;
+        }
+    }
+}
