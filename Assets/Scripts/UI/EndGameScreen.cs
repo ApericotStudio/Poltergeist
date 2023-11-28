0990762ac6c96e431cb3978077b9cdc7e225fd37
Jeroen <s1137642@student.hsleiden.nl>
2023-11-28 17:24:06 +0100

    feat: created an end game screen

---
+++ Assets/Scripts/UI/EndGameScreen.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameScreen : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameData _gameData;
+    [SerializeField] private TextMeshProUGUI _grade;
+    [SerializeField] private Button _replayButton;
+    
+    private void Awake()
+    {
+        _replayButton.onClick.AddListener(OnReplayButtonClicked);
+    }
+
+    public void OnEnable()
+    {
+        UpdateSummary();
+    }
+
+    private void UpdateSummary()
+    {
+        _grade.text = _gameData.Grade;
+    }
+
+    private void OnReplayButtonClicked()
+    {
+        SceneManager.LoadScene("game");
+    }
+}
