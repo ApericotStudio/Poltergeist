f92ff817f8e86d6e6a73a1d52d2ec3151e2b8b94
Jeroen <s1137642@student.hsleiden.nl>
2023-11-29 20:51:03 +0100

    feat: added timer for losing the game

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,13 +1,32 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    public UnityEvent OnEndGame = new UnityEvent();
+    [HideInInspector] public UnityEvent OnEndGame = new UnityEvent();
+
+    [Header("Adjustable variables")]
+    [SerializeField] private float _timeRemaining;
+
+    private void Awake()
+    {
+        StartCoroutine(ManageTimeRemaining());
+    }
 
     public void EndGame()
     {
         OnEndGame.Invoke();
         Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    IEnumerator ManageTimeRemaining()
+    {
+        while (_timeRemaining > 0)
+        {
+            _timeRemaining -= Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        EndGame();
     }
 }
