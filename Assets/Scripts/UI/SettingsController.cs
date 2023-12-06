237f9beaa6c02a4a3fec4a3cec02b751185b0cf7
Jeroen <s1137642@student.hsleiden.nl>
2023-12-06 11:49:35 +0100

    feat: added working sliders to settings canvas

--- Assets/Scripts/UI/SettingsController.cs
+++ Assets/Scripts/UI/SettingsController.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] private Slider _sensitivitySlider;
+    [SerializeField] private Slider _soundVolumeSlider;
+    [SerializeField] private Slider _musicVolumeSlider;
+    [SerializeField] private Button _backButton;
+    [Tooltip("Canvas that will be activated upon pressing the back button")]
+    [SerializeField] private GameObject _backDestinationCanvas;
+    [SerializeField] private SettingsData _settingsData;
+
+    private void Awake()
     {
-        
+        SetupButtonsAndSliders();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetupButtonsAndSliders()
     {
-        
+        _sensitivitySlider.onValueChanged.AddListener(OnSensitivitySliderValueChanged);
+        _soundVolumeSlider.onValueChanged.AddListener(OnSoundVolumeSliderValueChanged);
+        _musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeSliderValueChanged);
+        _backButton.onClick.AddListener(OnBackButtonPressed);
+    }
+
+    private void OnSensitivitySliderValueChanged(float value)
+    {
+        _settingsData.Sensitivity = value;
+    }
+
+    private void OnSoundVolumeSliderValueChanged(float value)
+    {
+        _settingsData.SoundVolume = value;
+    }
+
+    private void OnMusicVolumeSliderValueChanged(float value)
+    {
+        _settingsData.MusicVolume = value;
+    }
+
+    private void OnBackButtonPressed()
+    {
+        _backDestinationCanvas.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
