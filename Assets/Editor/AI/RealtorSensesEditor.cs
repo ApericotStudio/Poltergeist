a1fa0a4f513c07e095a559d8cb20b1f5bbefd77b
Dylan Smit <sircodeye@gmail.com>
2023-12-07 11:40:54 +0100

    Feat: realtor goes to investigate state now

--- Assets/Editor/AI/RealtorSensesEditor.cs
+++ Assets/Editor/AI/RealtorSensesEditor.cs
@@ -1,36 +1,36 @@
-using UnityEditor;
-using UnityEngine;
+// using UnityEditor;
+// using UnityEngine;
 
-[CustomEditor (typeof (RealtorSenses))]
-public class RealtorSensesEditor : Editor
-{
-    private RealtorSenses _realtorSenses;
+// [CustomEditor (typeof (RealtorSenses))]
+// public class RealtorSensesEditor : Editor
+// {
+//     private RealtorSenses _realtorSenses;
 
-    private void OnSceneGUI()
-    {
-        _realtorSenses = (RealtorSenses)target;
-        DrawDetectionRadius();
-        DrawDetectionLines();
-    }
+//     private void OnSceneGUI()
+//     {
+//         _realtorSenses = (RealtorSenses)target;
+//         DrawDetectionRadius();
+//         DrawDetectionLines();
+//     }
 
-    /// <summary>
-    /// Draws the realtor's detection radius on screen.
-    /// </summary>
-    private void DrawDetectionRadius()
-    {
-        Handles.color = Handles.color = new Color(0, 1, 0, 0.1f);
-        Handles.DrawSolidArc(_realtorSenses.transform.position, Vector3.up, Vector3.forward, 360, _realtorSenses.DetectionRange);
-    }
+//     /// <summary>
+//     /// Draws the realtor's detection radius on screen.
+//     /// </summary>
+//     private void DrawDetectionRadius()
+//     {
+//         Handles.color = Handles.color = new Color(0, 1, 0, 0.1f);
+//         Handles.DrawSolidArc(_realtorSenses.transform.position, Vector3.up, Vector3.forward, 360, _realtorSenses.FearReductionRange);
+//     }
 
-    /// <summary>
-    /// Draws a line from the realtor to the detected NPC. 
-    /// </summary>
-    private void DrawDetectionLines()
-    {
-        foreach (var npc in _realtorSenses.DetectedNpcs)
-        {
-            Handles.color = Color.blue;
-            Handles.DrawLine(_realtorSenses.transform.position, npc.transform.position);
-        }
-    }
-}
+//     /// <summary>
+//     /// Draws a line from the realtor to the detected NPC. 
+//     /// </summary>
+//     private void DrawDetectionLines()
+//     {
+//         foreach (var npc in _realtorSenses.DetectedNpcs)
+//         {
+//             Handles.color = Color.blue;
+//             Handles.DrawLine(_realtorSenses.transform.position, npc.transform.position);
+//         }
+//     }
+// }
