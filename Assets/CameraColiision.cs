bafd0b3572d8e53bc989acafa7286cdfb1641904
Hassan Mohamed <s1136140@student.hsleiden.nl>
2023-12-13 17:15:55 +0100

    fix: roof collision

--- Assets/CameraColiision.cs
+++ Assets/CameraColiision.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraColiision : MonoBehaviour
+public class CaneraCollision : MonoBehaviour
 {
-    public float collisionRadius = 0.5f;
+    public Transform lookAt;
+    public Transform camTransform;
 
-    void Update()
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivity = 4.0f;
+
+    void Start()
     {
+        camTransform = transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
         RaycastHit hit;
-        Vector3 offset = transform.forward * 0.1f;
-
-        if (Physics.Raycast(transform.position + offset, transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(lookAt.position, lookAt.position + rotation * dir, out hit, distance, layerMask))
         {
-
-            transform.position = hit.point - offset;
+            camTransform.position = lookAt.position + rotation * new Vector3(0, 0, -hit.distance);
         }
+        else
+        {
+            camTransform.position = lookAt.position + rotation * dir;
+        }
+        camTransform.LookAt(lookAt.position);
     }
 }
