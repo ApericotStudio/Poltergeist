f7e55822484922082b2edd5303334e78cf7ae30b
Jeroen <s1137642@student.hsleiden.nl>
2023-12-14 17:48:48 +0100

    feat: added feedback button to main menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -7,8 +7,11 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _settingsButton;
     [SerializeField] private Button _exitButton;
+    [SerializeField] private Button _feedbackButton;
     [SerializeField] private GameObject _levelSelectCanvas;
     [SerializeField] private GameObject _settingsCanvas;
+
+    private string _feedbackFormLink = "https://www.google.com/search?sca_esv=590933568&rlz=1C1VDKB_nlNL1073NL1073&sxsrf=AM9HkKkK5Zm03OJ3kJ9JTsjPTryE5VUblw:1702571829163&q=monke&tbm=isch&source=lnms&sa=X&ved=2ahUKEwj4q9Ooro-DAxVKhv0HHRtRADgQ0pQJegQIDxAB&biw=1280&bih=559&dpr=1.5#imgrc=U4fq2R8KOKmf_M";
 
     private void Awake()
     {
@@ -20,6 +23,7 @@ public class MainMenuController : MonoBehaviour
         _playButton.onClick.AddListener(OnPlayButtonPressed);
         _settingsButton.onClick.AddListener(OnSettingsButtonPressed);
         _exitButton.onClick.AddListener(OnExitButtonPressed);
+        _feedbackButton.onClick.AddListener(OnFeedbackButtonPressed);
     }
 
     private void OnPlayButtonPressed()
@@ -37,5 +41,10 @@ public class MainMenuController : MonoBehaviour
     private void OnExitButtonPressed()
     {
         Debug.Log("Exit Game");
+    }
+
+    private void OnFeedbackButtonPressed()
+    {
+        Application.OpenURL(_feedbackFormLink);
     }
 }
