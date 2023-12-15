220584a404edef9b7258681d9eebbd43d8b0f059
Jeroen <s1137642@student.hsleiden.nl>
2023-12-15 14:04:04 +0100

    change: feedback link is now a serialized field

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -11,7 +11,7 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject _levelSelectCanvas;
     [SerializeField] private GameObject _settingsCanvas;
 
-    private string _feedbackFormLink = "https://www.google.com/search?sca_esv=590933568&rlz=1C1VDKB_nlNL1073NL1073&sxsrf=AM9HkKkK5Zm03OJ3kJ9JTsjPTryE5VUblw:1702571829163&q=monke&tbm=isch&source=lnms&sa=X&ved=2ahUKEwj4q9Ooro-DAxVKhv0HHRtRADgQ0pQJegQIDxAB&biw=1280&bih=559&dpr=1.5#imgrc=U4fq2R8KOKmf_M";
+    [SerializeField] private string _feedbackFormLink = string.Empty;
 
     private void Awake()
     {
