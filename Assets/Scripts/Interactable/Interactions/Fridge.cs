08db317799221f2f3bea85ce508c16afd8e87e22
Dylan <95217570+SirCodeye@users.noreply.github.com>
2023-12-16 17:16:36 +0100

    Feat: added open closing sound to the fridge

---
+++ Assets/Scripts/Interactable/Interactions/Fridge.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Fridge : MonoBehaviour
+{
+    [Header("Fridge Settings")]
+    [Tooltip("Sound played when the fridge is opened."), SerializeField]
+    private AudioClip _openSound;
+    [Tooltip("Sound played when the fridge is closed."), SerializeField]
+    private AudioClip _closeSound;
+
+    private Animator _animator;
+    private Highlight _highlight;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _highlight = GetComponent<Highlight>();
+    }
+
+    public void Activate()
+    {
+        _animator.SetTrigger("Open");
+    }
+
+    private void OnOpen()
+    {
+        _highlight.Highlightable(false);
+        
+        if (_openSound != null)
+        AudioSource.PlayClipAtPoint(_openSound, transform.position);
+    }
+
+    private void OnClose()
+    {
+        _highlight.Highlightable(true);
+        AudioSource.PlayClipAtPoint(_closeSound, transform.position);
+    }
+}
