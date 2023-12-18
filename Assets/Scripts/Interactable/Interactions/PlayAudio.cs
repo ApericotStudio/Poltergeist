f9286a91ee199023958f84e9c3c615ef3d16a0e4
Hassan Mohamed <s1136140@student.hsleiden.nl>
2023-12-18 17:42:42 +0100

    feat: lantern added and mower

--- Assets/Scripts/Interactable/Interactions/PlayAudio.cs
+++ Assets/Scripts/Interactable/Interactions/PlayAudio.cs
@@ -14,12 +14,20 @@ public class PlayAudio : MonoBehaviour
     [SerializeField] private bool _keepProgress = false;
     [Tooltip("Check if item can be turned off or not")]
     [SerializeField] private bool _TurnOff = true;
+    [SerializeField] private bool _randomSound = true;
+
+    private int _numberClips;
+    private int counter = 0;
     [TextArea]
     public string DevNote = "Other adjustable variables can be found in the audio source component.";
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
+        if (_audioClips != null)
+        {
+            _numberClips = _audioClips.GetAmount();
+        }
     }
 
     public void Toggle()
@@ -28,7 +36,14 @@ public class PlayAudio : MonoBehaviour
         {
             if (_audioClips != null)
             {
-                _audioSource.clip = _audioClips.GetRandom();
+                if (_randomSound)
+                {
+                    _audioSource.clip = _audioClips.GetRandom();
+                }
+                else
+                {
+                    OnOff();
+                }
             }
             _audioSource.Play();
             return;
@@ -59,5 +74,15 @@ public class PlayAudio : MonoBehaviour
             }
             _audioSource.Play();
         }
+    }
+
+    public void OnOff()
+    {
+        if(counter == _numberClips)
+        {
+            counter = 0;
+        }
+        _audioSource.clip = _audioClips.GetIndex(counter);
+        counter++;
     }
 }
