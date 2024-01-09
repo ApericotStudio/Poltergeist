05c9cfaa69a9ec463bde78d15c156220c0daa6ed
Jeroen <s1137642@student.hsleiden.nl>
2024-01-09 14:37:03 +0100

    change: geist charge indicator is aimed towards player

--- Assets/Scripts/UI/GeistChargeIndicator.cs
+++ Assets/Scripts/UI/GeistChargeIndicator.cs
@@ -5,9 +5,20 @@ public class GeistChargeIndicator : MonoBehaviour
 {
     [SerializeField] private Image _filler;
 
+    private Vector3 _offset;
+    private Transform _playerCam;
+
     private void Awake()
     {
         GetComponentInParent<ObservableObject>().OnGeistChargeChanged += OnGeistChargeValueChange;
+        _offset = new Vector3(transform.localPosition.x, transform.localPosition.z, transform.localPosition.y);
+        _playerCam = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = transform.parent.position + _offset;
+        transform.LookAt(transform.position + _playerCam.forward);
     }
 
     private void OnGeistChargeValueChange(float value)
