7018e4f278922b50c95973f350d7b4db774a9535
Liang-Tong <kiwilt@icloud.com>
2024-01-09 15:30:47 +0100

    scare command works with the new object types

--- Assets/Scripts/Game/Console/Commands/ScareCommand.cs
+++ Assets/Scripts/Game/Console/Commands/ScareCommand.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Scare Command", menuName = "Utilities/ConsoleCommands/ScareCommand")]
 public class ScareCommand : ConsoleCommand
 {
+    [SerializeField] private FloatReference _sizeFearSmall;
+    [SerializeField] private FloatReference _sizeFearMedium;
+    [SerializeField] private FloatReference _sizeFearBig;
+
     public override bool Process(string[] args)
     {
         GameObject cheatScareObject = GameObject.Find("CheatScareObject");
@@ -14,16 +18,17 @@ public class ScareCommand : ConsoleCommand
         switch (args[0])
         {
             case "small":
-                observable.Type = ObjectType.Small;
+                observable.SizeFear = _sizeFearSmall;
                 break;
             case "medium":
-                observable.Type = ObjectType.Medium;
+                observable.SizeFear = _sizeFearMedium;
                 break;
             case "big":
-                observable.Type = ObjectType.Big;
+                observable.SizeFear = _sizeFearBig;
                 break;
             default:
-                return false;
+                observable.SizeFear = _sizeFearMedium;
+                break;
         }
         interactable.Use();
         observable.GeistCharge = 1;
