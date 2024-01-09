6658d54c53c82a85bedfa7c7a531a36fdda1a94c
Liang-Tong <kiwilt@icloud.com>
2024-01-09 16:34:22 +0100

    realtor disable command

---
+++ Assets/Scripts/Game/Console/Commands/RealtorDisableCommand.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Realtor Disable Command", menuName = "Utilities/ConsoleCommands/RealtorDisableCommand")]
+public class RealtorDisable : ConsoleCommand
+{
+    public override bool Process(string[] args)
+    {
+        GameObject realtor = GameObject.Find("RealtorArmature");
+        if (realtor == null) { return false; }
+        RealtorSenses senses = realtor.GetComponent<RealtorSenses>();
+        if (args.Length == 0){ senses.Disabled = true; }
+        if (args[0] == "false") { senses.Disabled = false; }
+        else { senses.Disabled = true; }
+        return true;
+    }
+}
