5cbef0626be2c0fbf762b33f4bc5bae9906baa63
Jeroen <s1137642@student.hsleiden.nl>
2024-01-15 15:32:10 +0100

    fix: audio no longer cuts of on scene switch

---
+++ Assets/Scripts/UI/Utility/FinishAudioOnLoad.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishAudioOnLoad : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        SceneManager.sceneUnloaded += FinishPlaying;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void FinishPlaying(Scene scene)
+    {
+        if (_audioSource.isPlaying)
+        {
+            Destroy(gameObject, _audioSource.clip.length);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
