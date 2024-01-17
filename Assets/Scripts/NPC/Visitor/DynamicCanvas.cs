8fb06b9db7ba8cdfcc27720d0cb6dc867359c92c
Dylan Smit <sircodeye@gmail.com>
2024-01-17 14:36:09 +0100

    WIP: Realtor aura

---
+++ Assets/Scripts/NPC/Visitor/DynamicCanvas.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DynamicCanvas : MonoBehaviour
+{
+    [Header("Dynamic Canvas Settings")]
+    [Tooltip("The transform that the canvas will follow."), SerializeField]
+    private Transform _transformToFollow;
+
+    private void Update()
+    {
+        RotateTowardsPlayer();
+        if(_transformToFollow != null)
+            FollowTransform();
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        Vector3 directionToCamera = Camera.main.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(-directionToCamera);
+    }
+
+    private void FollowTransform()
+    {
+        transform.position = _transformToFollow.position;
+    }
+}
