6d9e80f432ccd8450d6437fcce8990b007901af6
Liang-Tong <kiwilt@icloud.com>
2024-01-17 14:25:36 +0100

    GradHat appears when you have A for both assignment and exam

---
+++ Assets/Scripts/Game/ProgressionManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionManager : MonoBehaviour
+{
+    [SerializeField] GameObject GradHat;
+    LevelGradeHandler gradeHandler;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gradeHandler = new LevelGradeHandler();
+        Grade assignmentGrade = gradeHandler.Load("Assignment");
+        Grade examGrade = gradeHandler.Load("FinalExam");
+
+        if (assignmentGrade != null && examGrade != null)
+        {
+            if (assignmentGrade.Result == 4 && examGrade.Result == 4)
+            {
+                GetGradHat();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetGradHat()
+    {
+        GameObject headbone = GameObject.Find("headbone");
+        if (headbone != null)
+        {
+            GameObject coolHat = Instantiate(original: GradHat, parent: headbone.transform, position: headbone.transform.position, rotation: Quaternion.identity);
+            coolHat.transform.forward = headbone.transform.forward;
+        }
+    }
+}
